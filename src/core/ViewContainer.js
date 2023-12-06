10af4eeb368d57a9b4d2c6a942eb2234e966670e
jBreznar <jan.breznar2@um.si>
2023-12-06 13:36:44 +0100

    Trying loading images on canvas.

--- src/core/ViewContainer.js
+++ src/core/ViewContainer.js
@@ -1,5 +1,26 @@
 export class ViewContainer {
-    test() {
-        console.log("Test");
+
+    canvas;
+    canvas2DContext;
+
+    constructor(canvasId) {
+        this.canvas = document.querySelector(`#${canvasId}`);
+
+        if (this.canvas == null) {
+            console.error(`There is no element with id '${canvasId}'`);
+            return;
+        }
+
+        this.canvas2DContext = this.canvas.getContext("2d");
+    }
+
+    drawImage(imageUrl, offsetX, offsetY, width = this.canvas.width, height = this.canvas.height) {
+        const image = new Image();
+
+        image.onload = () => {
+            this.canvas2DContext.drawImage(image, offsetX, offsetY, width, height);
+        };
+
+        image.src = imageUrl;
     }
 }
